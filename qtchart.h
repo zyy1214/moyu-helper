c40f5a235708d89d4a6645281df880114fb9973e
yysmwt <liuyw05@163.com>
2024-05-25 23:31:35 +0800

    优化了柱状图，防止柱子过粗

--- qtchart.h
+++ qtchart.h
@@ -17,10 +17,18 @@
 #include "record.h"
 #include "mod.h"
 #include <QDebug>
+QString placeholder[5]={""," ","  ","   ","    "};
 QChartView* build_histogram_day(std::map<QDate, int> points_per_day){
     QBarSet *set0 = new QBarSet("Points Per Day");
+    int cnt=0;
     for(auto mr:points_per_day){
         *set0<<mr.second;
+        cnt++;
+    }
+    if(cnt!=0){
+        for(int i=cnt;i<5;i++){
+            *set0<<0;
+        }
     }
     QBarSeries *series = new QBarSeries();
     series->append(set0);
@@ -29,6 +37,11 @@ QChartView* build_histogram_day(std::map<QDate, int> points_per_day){
     QBarCategoryAxis *axisX = new QBarCategoryAxis();
     for(auto mr:points_per_day){
         axisX->append(mr.first.toString("dd MM yyyy"));
+    }
+    if(cnt!=0){
+        for(int i=cnt;i<5;i++){
+            axisX->append(placeholder[i]);
+        }
     }
     chart->addAxis(axisX, Qt::AlignBottom);
     series->attachAxis(axisX);
@@ -42,11 +55,13 @@ QChartView* build_histogram_month(std::map<QDate, int> points_per_day){
     QBarSet *set0 = new QBarSet("Points Per Month");
     int temp_point=0;
     int temp_month=0,temp_year=0;
+    int cnt=0;
     std::vector<QDate> y_date;
     if(!points_per_day.empty()){
         temp_month=points_per_day.begin()->first.month();
         temp_year=points_per_day.begin()->first.year();
         y_date.push_back(points_per_day.begin()->first);
+        cnt++;
     }
     for(auto mr:points_per_day){
         if(mr.first.month()==temp_month&&mr.first.year()==temp_year){
@@ -58,11 +73,17 @@ QChartView* build_histogram_month(std::map<QDate, int> points_per_day){
             temp_year=mr.first.year();
             temp_month=mr.first.month();
             y_date.push_back(mr.first);
+            cnt++;
         }
 
     }
     if(!y_date.empty()){
         *set0<<temp_point;
+    }
+    if(cnt!=0){
+        for(int i=cnt;i<5;i++){
+            *set0<<0;
+        }
     }
     QBarSeries *series = new QBarSeries();
     series->append(set0);
@@ -71,6 +92,11 @@ QChartView* build_histogram_month(std::map<QDate, int> points_per_day){
     QBarCategoryAxis *axisX = new QBarCategoryAxis();
     for(auto mr:y_date){
         axisX->append(mr.toString("MM yyyy"));
+    }
+    if(cnt!=0){
+        for(int i=cnt;i<5;i++){
+            axisX->append(placeholder[i]);
+        }
     }
     chart->addAxis(axisX, Qt::AlignBottom);
     series->attachAxis(axisX);
@@ -85,9 +111,11 @@ QChartView* build_histogram_year(std::map<QDate, int> points_per_day){
     int temp_point=0;
     int temp_year=0;
     std::vector<QDate> y_date;
+    int cnt=0;
     if(!points_per_day.empty()){
         temp_year=points_per_day.begin()->first.year();
         y_date.push_back(points_per_day.begin()->first);
+        cnt++;
     }
     for(auto mr:points_per_day){
         if(mr.first.year()==temp_year){
@@ -98,10 +126,16 @@ QChartView* build_histogram_year(std::map<QDate, int> points_per_day){
             temp_point=mr.second;
             temp_year=mr.first.year();
             y_date.push_back(mr.first);
+            cnt++;
         }
     }
     if(!y_date.empty()){
         *set0<<temp_point;
+    }
+    if(cnt!=0){
+        for(int i=cnt;i<5;i++){
+            *set0<<0;
+        }
     }
     QBarSeries *series = new QBarSeries();
     series->append(set0);
@@ -110,6 +144,11 @@ QChartView* build_histogram_year(std::map<QDate, int> points_per_day){
     QBarCategoryAxis *axisX = new QBarCategoryAxis();
     for(auto mr:y_date){
         axisX->append(mr.toString("yyyy"));
+    }
+    if(cnt!=0){
+        for(int i=cnt;i<5;i++){
+            axisX->append(placeholder[i]);
+        }
     }
     chart->addAxis(axisX, Qt::AlignBottom);
     series->attachAxis(axisX);
@@ -123,14 +162,23 @@ QChartView* build_histogram_day_all(std::map<QDate, int> points_per_day){
     QStackedBarSeries *series=new QStackedBarSeries();
     QBarSet *set0 = new QBarSet("Obtain Points Per Day");
     QBarSet *set1 = new QBarSet("Consume Points Per Day");
+    int cnt=0;
     for(auto mr:points_per_day){
         if(mr.second>=0){
             *set0<<mr.second;
             *set1<<0;
+            cnt++;
         }
         else{
             *set0<<0;
             *set1<<-mr.second;
+            cnt++;
+        }
+    }
+    if(cnt!=0){
+        for(int i=cnt;i<5;i++){
+            *set0<<0;
+            *set1<<0;
         }
     }
     series->append(set0);
@@ -140,6 +188,11 @@ QChartView* build_histogram_day_all(std::map<QDate, int> points_per_day){
     QBarCategoryAxis *axisX = new QBarCategoryAxis();
     for(auto mr:points_per_day){
         axisX->append(mr.first.toString("dd MM yyyy"));
+    }
+    if(cnt!=0){
+        for(int i=cnt;i<5;i++){
+            axisX->append(placeholder[i]);
+        }
     }
     chart->addAxis(axisX, Qt::AlignBottom);
     series->attachAxis(axisX);
@@ -155,11 +208,13 @@ QChartView* build_histogram_month_all(std::map<QDate, int> points_per_day){
     QBarSet *set1 = new QBarSet("Consume Points Per Month");
     int temp_point=0;
     int temp_month=0,temp_year=0;
+    int cnt=0;
     std::vector<QDate> y_date;
     if(!points_per_day.empty()){
         temp_month=points_per_day.begin()->first.month();
         temp_year=points_per_day.begin()->first.year();
         y_date.push_back(points_per_day.begin()->first);
+        cnt++;
     }
     for(auto mr:points_per_day){
         if(mr.first.month()==temp_month&&mr.first.year()==temp_year){
@@ -178,6 +233,7 @@ QChartView* build_histogram_month_all(std::map<QDate, int> points_per_day){
             temp_year=mr.first.year();
             temp_month=mr.first.month();
             y_date.push_back(mr.first);
+            cnt++;
         }
 
     }
@@ -191,6 +247,12 @@ QChartView* build_histogram_month_all(std::map<QDate, int> points_per_day){
             *set1<<-temp_point;
         }
     }
+    if(cnt!=0){
+        for(int i=cnt;i<5;i++){
+            *set0<<0;
+            *set1<<0;
+        }
+    }
     series->append(set0);
     series->append(set1);
     QChart *chart = new QChart();
@@ -198,6 +260,11 @@ QChartView* build_histogram_month_all(std::map<QDate, int> points_per_day){
     QBarCategoryAxis *axisX = new QBarCategoryAxis();
     for(auto mr:y_date){
         axisX->append(mr.toString("MM yyyy"));
+    }
+    if(cnt!=0){
+        for(int i=cnt;i<5;i++){
+            axisX->append(placeholder[i]);
+        }
     }
     chart->addAxis(axisX, Qt::AlignBottom);
     series->attachAxis(axisX);
@@ -213,10 +280,12 @@ QChartView* build_histogram_year_all(std::map<QDate, int> points_per_day){
     QBarSet *set1 = new QBarSet("Consume Points Per Year");
     int temp_point=0;
     int temp_year=0;
+    int cnt=0;
     std::vector<QDate> y_date;
     if(!points_per_day.empty()){
         temp_year=points_per_day.begin()->first.year();
         y_date.push_back(points_per_day.begin()->first);
+        cnt++;
     }
     for(auto mr:points_per_day){
         if(mr.first.year()==temp_year){
@@ -234,6 +303,7 @@ QChartView* build_histogram_year_all(std::map<QDate, int> points_per_day){
             temp_point=mr.second;
             temp_year=mr.first.year();
             y_date.push_back(mr.first);
+            cnt++;
         }
     }
     if(!y_date.empty()){
@@ -246,6 +316,12 @@ QChartView* build_histogram_year_all(std::map<QDate, int> points_per_day){
             *set1<<-temp_point;
         }
     }
+    if(cnt!=0){
+        for(int i=cnt;i<5;i++){
+            *set0<<0;
+            *set1<<0;
+        }
+    }
     series->append(set0);
     series->append(set1);
     QChart *chart = new QChart();
@@ -253,6 +329,11 @@ QChartView* build_histogram_year_all(std::map<QDate, int> points_per_day){
     QBarCategoryAxis *axisX = new QBarCategoryAxis();
     for(auto mr:y_date){
         axisX->append(mr.toString("yyyy"));
+    }
+    if(cnt!=0){
+        for(int i=cnt;i<5;i++){
+            axisX->append(placeholder[i]);
+        }
     }
     chart->addAxis(axisX, Qt::AlignBottom);
     series->attachAxis(axisX);
